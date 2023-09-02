44b92bff675a68a72d85d6ff795784e6ef6fabda
Matthew Peters <matthew@datadelve.net>
2023-09-02 16:11:18 -0700

    support input of multiple players

--- cmd/goJack/main.go
+++ cmd/goJack/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/matthewapeters/gojack/pkg/game"
 )
 
 func main() {
+	names := []string{}
+	stdinReader := bufio.NewScanner(os.Stdin)
 	game.ClearScreen()
 	game.TitleArt()
-	n := ""
-	fmt.Print("What is your name? ")
-	fmt.Scanln(&n)
+	fmt.Print("List Players: ")
+	stdinReader.Scan()
+	input := stdinReader.Text()
+	for _, n := range strings.Split(input, " ") {
+		names = append(names, n)
+	}
 	game.ClearScreen()
-	game.Play(n)
+	game.Play(names...)
 }
